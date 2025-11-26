af3e1925acd9d24b71eba33eae20620677e9a35f
MartinEdu <MeWhenTheMartin@users.noreply.github.com>
2025-11-26 15:29:36 +0400

    Ex 3 Python Script
    Ex 3 Python Script

--- Assessment 1 - Skills Portfolio/Marks/StudentManager.py
+++ Assessment 1 - Skills Portfolio/Marks/StudentManager.py
@@ -1,19 +1,44 @@
-#Searching in the file 
-with open("_Resources/studentMarks.txt") as file_handler:
-   	lines = file_handler.readlines() 
-# Empty lists to copy values from file
-name = []
-age = []
-gender = []
-#Read the individual lines from file
-for l in lines:
-   	data = l.split(',')
-   	# Use split() to retrieve individual values separated by delimiter
-   	#Append the values of data list to individual lists
-	name.append((data[0]))
-	age.append((data[1]))
-	gender.append((data[2].replace("\n", "")))   	
- 
-print("Name list \n", name)
-print("Age list \n", age)
-print("Gender list \n", gender)
+file_path = '_Resources/studentMarks.txt'  # Example: data.txt contains "ID, NAME, SCORES"
+                                           # ['1345', 'John Curry', '8', '15', '7', '45']
+data = []
+studentData = []
+current = []
+
+with open(file_path, 'r') as file:
+    for line in file:
+        # Remove leading/trailing whitespace and split by comma
+        elements = line.strip().split(',')
+        data.append(elements)
+
+# DEBUG
+#print(f"First Student: {data[1]}")
+#print(f"Name: {data[1]} Student ID: {data[0]}")
+#print(f"Scores: {data[2]}, {data[3]}, {data[4]}, {data[5]}")
+
+def readStudent(studentNum):
+
+    global studentData
+
+    studentData = data[studentNum]
+    ID = studentData[0]
+    Name = studentData[1]
+    Scores = studentData[2:5]
+
+    print(f"ID: {ID} Name: {Name}\n Scores: {Scores}")
+
+def studentPage(page):
+
+    pageNum = page * 2
+    count = pageNum
+    print(f"\nPAGE NO.{page}\n")
+
+    for _ in range(5):
+        count += 1
+        readStudent(count)
+
+studentPage(1)
+studentPage(2)
+    
+
+# Main Window
+from tkinter import * 
